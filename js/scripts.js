99028fc2a1b97709d16be2a3b848d8c8c77cb8dd
Tanner Read <tread06@gmail.com>
2021-08-09 13:11:28 -0400

    Added type testing and filtering to the pokemon repository
    Added a check to make sure that the pokemon object contains all the correct keys. Added a function to retrieve only one pokemon by name. Moved the write functionality into its own function for reuse.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 
-
 let pokemonRepository = (function() {
 
     let pokemonList = [
@@ -9,33 +8,74 @@ let pokemonRepository = (function() {
     ];
 
     function add(pokemon){
-        pokemonList.push(pokemon);
+        /*check if the pokemon param contains all properties.
+        if only 3 properties exist, and that they are the correct types */
+        if(typeof(pokemon === Object))
+        {
+            if( typeof(pokemon.name) === 'string' &&
+                typeof(pokemon.height) === 'number' &&
+                typeof(pokemon.types) === 'object' &&
+                Object.keys(pokemon).length === 3)
+            {
+                pokemonList.push(pokemon);
+            }
+            else
+            {
+                console.warn('Pokemon is invalid. Cannot add to pokemon repository.');
+            }
+        }
     }
     function getAll(){
         return pokemonList;
     }
+    function get(name){
+        return pokemonList.find(pokemon => pokemon.name === name);
+    }
     return{
         add:add,
-        getAll:getAll
+        getAll:getAll,
+        get:get
     };
 })();
 
-//add a couple pokemon for testing
+//adding a couple pokemon to test out the add function
 pokemonRepository.add({name: 'Pikachu', height: 0.4, types: ['Electric']});
 pokemonRepository.add({name: 'Jigglypuff', height: 0.5, types: ['Normal', 'Fairy']});
 
-document.write('<h2>Pokemon Entries</h2>');
-pokemonRepository.getAll().forEach( pokemon => {
+//test adding invalid pokemon
+pokemonRepository.add({name: 123, height: "0.5", types: ['Normal', 'Fairy']});
+pokemonRepository.add({name: "Charizard", extraParam: "invalid", height: 1.5, types: ['Fire']});
+pokemonRepository.add({height: 0.4, types: ['Electric']});
+
+//moved write into a new function since it's used in two places
+function writePokemon(pokemon)
+{
+    if(pokemon===undefined) {
+        return;
+    }    
+
     document.write(`<p>${pokemon.name} \(${pokemon.height}\m) `);
 
-    //add highlight for pokemon larger than 0.5m
+    //add text highlight for pokemon larger than 0.5m
     if(pokemon.height > 0.5){
         document.write('Wow, That\'s big!');
     }
-
-    //add highlight for pokemon smaller than 0.5m
+    //add text highlight for pokemon smaller than 0.5m
     if(pokemon.height < 0.5){
         document.write('So tiny!');
     }
     document.write('</p>');
+}
+
+//write pokemon list
+document.write('<h2>Pokemon Entries</h2>');
+pokemonRepository.getAll().forEach( pokemon => {
+    writePokemon(pokemon);
 });
+//write specific pokemon
+document.write('<h2>Get Specific Pokemon</h2>');
+writePokemon(pokemonRepository.get('Pikachu'));
+writePokemon(pokemonRepository.get('Charmander'));
+
+//test writing a specific pokemon that does not exist
+writePokemon(pokemonRepository.get('Charizard'));
