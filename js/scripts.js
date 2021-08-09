23b86cb0c4ae486ab90941e024bfee9fcb72b3f3
Tanner Read <tread06@gmail.com>
2021-08-09 12:04:29 -0400

    Refactored pokemon list to use IIFE
    Added pokemon repository. Changed For loop into a foreach loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,39 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, types: ['Grass', 'Poison']},
-    {name: 'Charmander', height: 0.6, types: ['Fire']},
-    {name: 'Squirtle', height: 0.5, types: ['Water']},
-    {name: 'Jigglypuff', height: 0.5, types: ['Normal', 'Fairy']},
-    {name: 'Pikachu', height: 0.4, types: ['Electric']}
-];
+
+
+let pokemonRepository = (function() {
+
+    let pokemonList = [
+        {name: 'Bulbasaur', height: 0.7, types: ['Grass', 'Poison']},
+        {name: 'Charmander', height: 0.6, types: ['Fire']},
+        {name: 'Squirtle', height: 0.5, types: ['Water']},
+        {name: 'Jigglypuff', height: 0.5, types: ['Normal', 'Fairy']},
+        {name: 'Pikachu', height: 0.4, types: ['Electric']}
+    ];
+    
+    function add(pokemon){
+        pokemonList.push(pokemon);
+    }
+    function getAll(){
+        return pokemonList;
+    }
+    return{
+        add:add,
+        getAll:getAll
+    };
+})();
 
 document.write('<h2>Pokemon Entries</h2>');
-for(let i=0;i<pokemonList.length;i++){
-    
-    document.write(`<p>${pokemonList[i].name} \(${pokemonList[i].height}\m) `);
+pokemonRepository.getAll().forEach( pokemon => {
+    document.write(`<p>${pokemon.name} \(${pokemon.height}\m) `);
 
     //add highlight for pokemon larger than 0.5m
-    if(pokemonList[i].height > 0.5){
+    if(pokemon.height > 0.5){
         document.write('Wow, That\'s big!');
     }
 
     //add highlight for pokemon smaller than 0.5m
-    if(pokemonList[i].height < 0.5){
+    if(pokemon.height < 0.5){
         document.write('So tiny!');
     }
     document.write('</p>');
-}
+});
