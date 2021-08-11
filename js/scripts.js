850bcf452f614252b92bcad6bce41e3e7e164362
Tanner Read <tread06@gmail.com>
2021-08-11 14:32:13 -0400

    Updated formatting in scripts.js
    Made a few minor formatting tweaks.

--- js/scripts.js
+++ js/scripts.js
@@ -1,76 +1,81 @@
-
-let pokemonRepository = (function() {
-
+let pokemonRepository = (function () {
+    
     let pokemonList = [];
-    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
-    function add(pokemon){        
+    function add(pokemon) {
         pokemonList.push(pokemon);
     }
-    function getAll(){
+    function getAll() {
         return pokemonList;
     }
-    function get(name){
-        return pokemonList.find(pokemon => pokemon.name === name);
+    function get(name) {
+        return pokemonList.find((pokemon) => pokemon.name === name);
     }
-    function addListItem(pokemon){
-        let listParent = document.querySelector('.pokemon-list');
-        let listItem = document.createElement('li');
-        let button = document.createElement('button');
+    function addListItem(pokemon) {
+        let listParent = document.querySelector(".pokemon-list");
+        let listItem = document.createElement("li");
+        let button = document.createElement("button");
         button.innerText = pokemon.name;
-        button.classList.add('pokemon-list__button');        
+        button.classList.add("pokemon-list__button");
         listItem.appendChild(button);
         listParent.appendChild(listItem);
-        addPokemonClickListener(button,pokemon);
-    }   
-    function showDetailsEvent(){
+        addPokemonClickListener(button, pokemon);
+    }
+    function showDetailsEvent() {
         loadDetails(this).then(function (pokemon) {
-            console.log(pokemon);
+        console.log(pokemon);
         });
     }
     function addPokemonClickListener(button, pokemon) {
-        button.addEventListener('click', showDetailsEvent.bind(pokemon));
-    } 
+        button.addEventListener("click", showDetailsEvent.bind(pokemon));
+    }
     function loadList() {
-        return fetch(apiUrl).then(function (response) {
+        return fetch(apiUrl)
+        .then(function (response) {
             return response.json();
-        }).then(function (json) {
+        })
+        .then(function (json) {
             json.results.forEach(function (item) {
             let pokemon = {
                 name: item.name,
-                detailsUrl: item.url
+                detailsUrl: item.url,
             };
             add(pokemon);
             });
-        }).catch(function (e) {
-            console.error(e);
         })
-    }   
+        .catch(function (e) {
+            console.error(e);
+        });
+    }
     function loadDetails(item) {
         let url = item.detailsUrl;
-        return fetch(url).then(function (response) {            
+        return fetch(url)
+        .then(function (response) {
             return response.json();
-        }).then(function (details) {            
+        })
+        .then(function (details) {
             item.imageUrl = details.sprites.front_default;
             item.height = details.height;
             item.types = details.types;
             return details;
-        }).catch(function (e) {
+        })
+        .catch(function (e) {
             console.error(e);
         });
     }
-    return{
-        add:add,
-        getAll:getAll,
-        get:get,
-        addListItem:addListItem,
-        loadList:loadList,
-        loadDetails:loadDetails
+    return {
+        add: add,
+        getAll: getAll,
+        get: get,
+        addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
     };
 })();
 
-pokemonRepository.loadList().then(function() {    
-    pokemonRepository.getAll().forEach(function(pokemon){
-    pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
     });
 });
